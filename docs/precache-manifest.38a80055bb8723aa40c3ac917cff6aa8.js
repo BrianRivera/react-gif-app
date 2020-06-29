10722fdc755bf686ea9bedfa5db0a07067dbdc52
brian <brian.rivera.proboste@gmail.com>
2020-06-28 23:24:31 -0400

    docs del build

---
+++ docs/precache-manifest.38a80055bb8723aa40c3ac917cff6aa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "456d548ca11112f2b6e5aa7263ff3a27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d62e84ccf8b508fd8866",
+    "url": "/static/css/main.bafba144.chunk.css"
+  },
+  {
+    "revision": "f4af51b44e69e37dc93c",
+    "url": "/static/js/2.63435044.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.63435044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d62e84ccf8b508fd8866",
+    "url": "/static/js/main.eb9f913b.chunk.js"
+  },
+  {
+    "revision": "abd056ceb34337b5356c",
+    "url": "/static/js/runtime-main.6cdc96cd.js"
+  }
+]);
